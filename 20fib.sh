5bdb443c20298723dc66fb7e7aef0435d83b13bd
plantationboss <plantationboss77@gmail.com>
2025-03-06 12:01:45 +0530

    Add files via upload

---
+++ 20fib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+fibonacci() {
+n=$1
+a=0
+b=1
+echo "fibonacci Series up to $n terms:"
+for (( i=0; i<n; i++ ))
+do
+echo -n "$a"
+temp=$((a + b))
+a=$b
+b=$temp
+done
+echo
+}
+read -p "Enter the number of terms:" num
+fibonacci $num
+ 
